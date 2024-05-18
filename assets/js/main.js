80ae8d1a5f76306ac585b0a411c10f8a0b037a49
IRADUKUNDA4 <thierryiradukunda1998@gmail.com>
2024-05-18 20:41:18 +0200

    feature : update amount

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,142 @@
+// // add hovered class to selected list item
+// let list = document.querySelectorAll(".navigation li");
+// let button = document.querySelectorAll(".signup-btn");
+// let amount = document.getElementById("amount");
+
+
+// let toggle = document.querySelector(".toggle");
+// let navigation = document.querySelector(".navigation");
+// let main = document.querySelector(".main");
+
+// toggle.onclick = function () {
+//   navigation.classList.toggle("active");
+//   main.classList.toggle("active");
+// };
+
+// let storedBlogs = [];
+// function fetchDataAndDisplay() {
+//   fetch('https://weak-gold-llama-wrap.cyclic.app/signup')
+//  .then(response => response.json())
+//  .then(async (resp) => {
+//       storedBlogs = await resp.data;
+//       localStorage.setItem('all_users', JSON.stringify(storedBlogs));
+
+//       if (storedBlogs) {
+//           console.log("storedBlogs : ");
+//           storedBlogs.forEach(user => {
+//               console.log("each user : ");
+//               console.log(user);
+//               const row = document.createElement('tr');
+          
+//               const cardNoCell = document.createElement('td');
+//               cardNoCell.textContent = user.username;
+//               row.appendChild(cardNoCell);
+          
+//               const priceCell = document.createElement('td');
+//               priceCell.textContent = user.wallet;
+//               row.appendChild(priceCell);
+          
+//               const statusSpan = document.createElement('span');
+//               statusSpan.className = `status`;
+//               statusSpan.textContent = user.email;
+//               const statusCell = document.createElement('td');
+//               statusCell.appendChild(statusSpan);
+//               row.appendChild(statusCell);
+//               row.addEventListener('click', () => {
+//                 window.location.href = `/Smart-public-toilet-door-management-system/sendtocard.html?id=${user.username}`;
+//             });
+//               table.querySelector('tbody').appendChild(row);
+//           });
+//       }
+//   });
+// }
+
+// window.onload = fetchDataAndDisplay;
+
+
+// // fetch('https://weak-gold-llama-wrap.cyclic.app/signup')
+// // .then(response => response.json())
+// // .then(async (resp) => {
+// //     storedBlogs = await resp.data;
+// //     localStorage.setItem('all_users', JSON.stringify(storedBlogs));
+
+// //     if (storedBlogs) {
+// //         console.log("storedBlogs : ");
+// //         storedBlogs.forEach(user => {
+// //             console.log("each user : ");
+// //             console.log(user);
+// //             const row = document.createElement('tr');
+        
+// //             const cardNoCell = document.createElement('td');
+// //             cardNoCell.textContent = user.username;
+// //             row.appendChild(cardNoCell);
+        
+// //             const priceCell = document.createElement('td');
+// //             priceCell.textContent = user.wallet;
+// //             row.appendChild(priceCell);
+        
+// //             const statusSpan = document.createElement('span');
+// //             statusSpan.className = `status`;
+// //             statusSpan.textContent = user.email;
+// //             const statusCell = document.createElement('td');
+// //             statusCell.appendChild(statusSpan);
+// //             row.appendChild(statusCell);
+// //             // Append the row to the table body
+// //             row.addEventListener('click', () => {
+// //               // alert("this row is clicked");
+              
+// //               window.location.href = `/sendtocard.html?id=${user.username}`;
+
+// //           });
+// //             table.querySelector('tbody').appendChild(row);
+// //         });
+// //     }
+// // });
+// const table = document.querySelector('.details table');
+
+// function getIdFromUrl() {
+//   const urlParams = new URLSearchParams(window.location.search);
+//   return urlParams.get('id');
+// }
+
+// addAmount.addEventListener('click', function(event) {
+//   event.preventDefault(); // Prevent the form from submitting normally
+
+// const id = getIdFromUrl();
+
+// const url = `https://weak-gold-llama-wrap.cyclic.app/transact/?id=${id}&op=add&amount=${amount.value}`;
+// // const url = `http://handsome-puce-snapper.cyclic.app/transact/?id=${id}&op=sub&amount=${amount.value}`;
+// let  updateWallet = async () => {
+  
+// try {
+//     // Send the POST request
+//     const response = await fetch(url, {
+//         method: 'PATCH',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//     });
+
+//     if (!response.ok) {
+//       alert('error');
+//         throw new Error(`HTTP error status: ${response.status}`);
+//     }
+//     alert('Amount added successfully');
+//     window.location.href = `/Smart-public-toilet-door-management-system/index.html`;
+
+// } catch (error) {
+//     console.error('Error:', error);
+// }}
+// updateWallet();
+
+// });
+
 // add hovered class to selected list item
 let list = document.querySelectorAll(".navigation li");
 let button = document.querySelectorAll(".signup-btn");
 let amount = document.getElementById("amount");
-
+let numbersDiv = document.getElementById("numbers");
+let usersDiv = document.getElementById("users");
 
 let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
@@ -15,46 +149,49 @@ toggle.onclick = function () {
 
 let storedBlogs = [];
 function fetchDataAndDisplay() {
-  fetch('https://weak-gold-llama-wrap.cyclic.app/signup')
- .then(response => response.json())
- .then(async (resp) => {
+  fetch("http://weak-gold-llama-wrap.cyclic.app/signup")
+    .then((response) => response.json())
+    .then(async (resp) => {
+      numbersDiv.innnerHTML = "user.wallet";
       storedBlogs = await resp.data;
-      localStorage.setItem('all_users', JSON.stringify(storedBlogs));
+      // storedBlogsNumber = await storedBlogs.message;
+      localStorage.setItem("all_users", JSON.stringify(storedBlogs));
 
       if (storedBlogs) {
-          console.log("storedBlogs : ");
-          storedBlogs.forEach(user => {
-              console.log("each user : ");
-              console.log(user);
-              const row = document.createElement('tr');
-          
-              const cardNoCell = document.createElement('td');
-              cardNoCell.textContent = user.username;
-              row.appendChild(cardNoCell);
-          
-              const priceCell = document.createElement('td');
-              priceCell.textContent = user.wallet;
-              row.appendChild(priceCell);
-          
-              const statusSpan = document.createElement('span');
-              statusSpan.className = `status`;
-              statusSpan.textContent = user.email;
-              const statusCell = document.createElement('td');
-              statusCell.appendChild(statusSpan);
-              row.appendChild(statusCell);
-              row.addEventListener('click', () => {
-                window.location.href = `/Smart-public-toilet-door-management-system/sendtocard.html?id=${user.username}`;
+        usersDiv.textContent = `${storedBlogs.length - 1}`;
+        storedBlogs.forEach((user) => {
+          if (user.username !== "Admin") {
+            const row = document.createElement("tr");
+            const cardNoCell = document.createElement("td");
+            cardNoCell.textContent = user.username;
+            row.appendChild(cardNoCell);
+
+            const priceCell = document.createElement("td");
+            priceCell.textContent = user.wallet;
+            row.appendChild(priceCell);
+
+            const statusSpan = document.createElement("span");
+            statusSpan.className = status;
+            statusSpan.textContent = user.email;
+            const statusCell = document.createElement("td");
+            statusCell.appendChild(statusSpan);
+            row.appendChild(statusCell);
+            row.addEventListener("click", () => {
+              window.location.href = `/Smart-public-toilet-door-management-system/sendtocard.html?id=${user.username}`;
+              // window.location.href = /public-fn/sendtocard.html?id=${user.username};
             });
-              table.querySelector('tbody').appendChild(row);
-          });
+            table.querySelector("tbody").appendChild(row);
+          } else {
+            numbersDiv.textContent = `${user.wallet} Rwf`;
+          }
+        });
       }
-  });
+    });
 }
 
 window.onload = fetchDataAndDisplay;
 
-
-// fetch('https://weak-gold-llama-wrap.cyclic.app/signup')
+// fetch('http://weak-gold-llama-wrap.cyclic.app/signup')
 // .then(response => response.json())
 // .then(async (resp) => {
 //     storedBlogs = await resp.data;
@@ -66,17 +203,17 @@ window.onload = fetchDataAndDisplay;
 //             console.log("each user : ");
 //             console.log(user);
 //             const row = document.createElement('tr');
-        
+
 //             const cardNoCell = document.createElement('td');
 //             cardNoCell.textContent = user.username;
 //             row.appendChild(cardNoCell);
-        
+
 //             const priceCell = document.createElement('td');
 //             priceCell.textContent = user.wallet;
 //             row.appendChild(priceCell);
-        
+
 //             const statusSpan = document.createElement('span');
-//             statusSpan.className = `status`;
+//             statusSpan.className = status;
 //             statusSpan.textContent = user.email;
 //             const statusCell = document.createElement('td');
 //             statusCell.appendChild(statusSpan);
@@ -84,50 +221,48 @@ window.onload = fetchDataAndDisplay;
 //             // Append the row to the table body
 //             row.addEventListener('click', () => {
 //               // alert("this row is clicked");
-              
-//               window.location.href = `/sendtocard.html?id=${user.username}`;
+
+//               window.location.href = /sendtocard.html?id=${user.username};
 
 //           });
 //             table.querySelector('tbody').appendChild(row);
 //         });
 //     }
 // });
-const table = document.querySelector('.details table');
+const table = document.querySelector(".details table");
 
 function getIdFromUrl() {
   const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get('id');
+  return urlParams.get("id");
 }
 
-addAmount.addEventListener('click', function(event) {
+addAmount.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent the form from submitting normally
 
-const id = getIdFromUrl();
+  const id = getIdFromUrl();
 
-const url = `https://weak-gold-llama-wrap.cyclic.app/transact/?id=${id}&op=add&amount=${amount.value}`;
-// const url = `http://handsome-puce-snapper.cyclic.app/transact/?id=${id}&op=sub&amount=${amount.value}`;
-let  updateWallet = async () => {
-  
-try {
-    // Send the POST request
-    const response = await fetch(url, {
-        method: 'PATCH',
+  const url = `http://weak-gold-llama-wrap.cyclic.app/transact/?id=${id}&op=add&amount=${amount.value}`;
+  // const url = http://handsome-puce-snapper.cyclic.app/transact/?id=${id}&op=sub&amount=${amount.value};
+  let updateWallet = async () => {
+    try {
+      // Send the POST request
+      const response = await fetch(url, {
+        method: "PATCH",
         headers: {
-            'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-    });
+      });
 
-    if (!response.ok) {
-      alert('error');
+      if (!response.ok) {
+        alert("error");
         throw new Error(`HTTP error status: ${response.status}`);
+      }
+      alert("Amount added successfully");
+      window.location.href = "/Smart-public-toilet-door-management-system/index.html";
+      // window.location.href = /public-fn/index.html;
+    } catch (error) {
+      console.error("Error:", error);
     }
-    alert('Amount added successfully');
-    window.location.href = `/Smart-public-toilet-door-management-system/index.html`;
-
-} catch (error) {
-    console.error('Error:', error);
-}}
-updateWallet();
-
+  };
+  updateWallet();
 });
-
